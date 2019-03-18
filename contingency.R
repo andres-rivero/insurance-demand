4f9e96b0179be1b8b98e813fa5a77b0fa5603fef
andresr91 <andres59@gmail.com>
2019-03-18 17:20:44 -0600

    a

--- contingency.R
+++ contingency.R
@@ -1,1 +1,16 @@
 data <- read_csv("data.csv")
+
+
+purchased <- function(x){
+  if (x == "cerrado"){
+    return(1)
+  }
+  else if (x == "aceptado"){
+    return(1)
+  }
+  else {
+    return(0)
+  }
+}
+
+data$outcome <- sapply(data$estatus, FUN = purchased)
