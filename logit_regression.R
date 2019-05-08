48697d3096b74ff5c8f249d1b24f9cbecb8e1ea2
andresr91 <andres59@gmail.com>
2019-05-08 01:14:27 -0500

    split data, test model

--- logit_regression.R
+++ logit_regression.R
@@ -1,6 +1,21 @@
 library(aod)
 library(ggplot2)
+library(caret)
+library(InformationValue)
 
-logit <- glm(outcome ~ auto + db_year, data = data, family = "binomial")
+
+set.seed(3456)
+trainIndex <- createDataPartition(data$outcome, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+
+dataTrain <- data[ trainIndex,]
+dataTest  <- data[-trainIndex,]
+
+
+logit <- glm(outcome ~ auto + db_year, data = dataTrain, family = "binomial")
 
 summary(logit)
+
+
+predicted <- predict(logit, dataTest, type="response")
